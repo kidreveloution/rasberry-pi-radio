cff19d1d9250026bc76399a3ba032bb28a5102d6
infinumtech <alyeldinshahin@gmail.com>
2022-04-04 12:52:10 -0400

    more errors, sigh

--- pythonMain.py
+++ pythonMain.py
@@ -1,9 +1,9 @@
 from multiprocessing import Process, Queue
-from radioFile import *
 import subprocess
 from datetime import datetime
 import time
 import requests
+from radioFile import *
 
 
     
@@ -13,25 +13,18 @@ def ApiChecker():
     time.sleep(1)
     tempTrack = 1
     while True:
-        response = requests.get("https://t06t4i.deta.dev/currentTrack")
-        currentTrack = int(response.text)
+        #response = requests.get("https://t06t4i.deta.dev/currentTrack")
+        currentTrack = 2
         time.sleep(1)
+        #intTrack = int(currentTrack)
         print(currentTrack)
+
         if tempTrack != currentTrack:
             tempTrack = currentTrack
             
             subprocess.Popen(['pkill', '-9', 'ffplay'])
             subprocess.Popen(['python', 'radioPlay.py', str(tempTrack)])
 
-
-
-def playFunction(num):
-
-    p2 = Process(target=currentlyPlaying ,args=(num,))
-
-    p2.start()
-    p2.join()
-    
 
 
 if __name__ == '__main__':
